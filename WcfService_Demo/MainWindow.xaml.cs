6d603ee9e9a74a2aeb3dc717883a2b38510146b8
Ken Luo <ken670128@hotmail.com>
2018-07-09 19:47:33 +0800

    Update

--- WcfService_Demo/MainWindow.xaml.cs
+++ WcfService_Demo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ServiceModel.Web;
 using System.Windows;
 
@@ -19,6 +20,8 @@ namespace WcfService_Demo
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             host = new WebServiceHost(typeof(Service1), new Uri("http://localhost:8000/"));
+            Service1.label1 = this.label1;
+            Service1.songlist = songlist;
             host.Open();
         }
 
@@ -26,5 +29,20 @@ namespace WcfService_Demo
         {
             host.Close();
         }
+
+        public static List<string> songlist = new List<string>()
+        {
+            "101753|國語|女歌星|吳佩慈|閃著淚光的決定[滾石]",
+            "125575|國語|團體|法蘭黛樂團|閃電 [瑞影]",
+            "122264|國語|團體|M3|閃電[弘音](music)",
+            "117118|國語|團體|四分衛|閃電[揚聲]",
+            "202845|台語|女歌星|徐紫淇|閃辣閃辣[音圓]",
+            "204304|台語|女歌星|葉璦菱|閃爍的天星[美華]",
+            "204305|台語|女歌星|葉璦菱|閃爍的天星[乾坤]",
+            "300633|粵語|女歌星|楊千嬅|閃靈[新藝寶+加州紅]",
+            "205006|台語|女歌星|蔡秋鳳|陣陣的腳步聲[你歌]",
+            "204858|台語|女歌星|劉俐婷|陣陣風雨[美華]"
+        };
+
     }
 }
--- WcfService_Demo/Service1.cs
+++ WcfService_Demo/Service1.cs
@@ -1,21 +1,29 @@
-﻿using System.IO;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Windows.Controls;
 
 namespace WcfService_Demo
 {
     // 注意: 您可以使用 [重構] 功能表上的 [重新命名] 命令同時變更程式碼和組態檔中的類別名稱 "Service1"。
     public class Service1 : IService1
     {
+        public static Label label1 { get; set; }
+        public static List<string> songlist { get; set; }
+
         public Stream QuerySong(string lang, string singer, string words, string condition, int page, int rows, string sort)
         {
-            string text = (condition != null) ? "{\"Song_Id\": 10015,\"Song_SongName\": \"Flow[揚聲]\",\"Song_Singer\": \"方大同 & 王力宏\",\"Song_WordCount\": 1,\"Song_Lang\": \"國語\",\"Song_SongStroke\": 1}" : "[]";
+            label1.Content = (condition != null && condition != "") ? "狀態: 查詢歌曲(" + condition + ")" : "狀態: 無";
+            string text = (condition != null && condition != "") ? JsonConvert.SerializeObject(songlist, Formatting.None) : "[]";
             WebOperationContext.Current.OutgoingResponse.ContentType = "application/json; charset=utf-8";
             return new MemoryStream(Encoding.UTF8.GetBytes(text));
         }
 
         public Stream QuerySinger(string condition, int page, int rows, string sort)
         {
+            label1.Content = (condition != null && condition != "") ? "狀態: 查詢歌手(" + condition + ")" : "狀態: 無";
             string text = "{\"Singer_Name\":\"AKI黃淑惠\",\"Singer_Type\":1,\"Singer_Strokes\":1}";
             WebOperationContext.Current.OutgoingResponse.ContentType = "application/json; charset=utf-8";
             return new MemoryStream(Encoding.UTF8.GetBytes(text));
@@ -23,6 +31,7 @@ namespace WcfService_Demo
 
         public Stream DoCrazyKTV_Action(string value, string state)
         {
+            label1.Content = "狀態: 點歌功能(切歌)";
             string text = "{\"點歌功能\":\"切歌\"}";
             WebOperationContext.Current.OutgoingResponse.ContentType = "application/json; charset=utf-8";
             return new MemoryStream(Encoding.UTF8.GetBytes(text));
@@ -30,6 +39,7 @@ namespace WcfService_Demo
 
         public Stream DoCrazyKTV_Control(int value, string state)
         {
+            label1.Content = "狀態: 播放控制(靜音)";
             string text = "{\"播放控制\":\"靜音\"}";
             WebOperationContext.Current.OutgoingResponse.ContentType = "application/json; charset=utf-8";
             return new MemoryStream(Encoding.UTF8.GetBytes(text));
